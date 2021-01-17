6cfe2d46d73d0604c1a02c0c429c7380c239242c
FabioCapobianchi <49325693+FabioCapobianchi@users.noreply.github.com>
2021-01-17 17:59:13 +0000

    fim

--- Trabalho de IIA2020/main.c
+++ Trabalho de IIA2020/main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
     init_rand();
     //psol ps = NULL;
     pdad pd = NULL;
-    int mbf = 0, mbf_aux = 0, bestmbf = 0, bestbest = 0, max_val = 0;
+    int mbf = 0, mbf_aux = 0, bestmbf = 0, bestbest = 0;
     int i, ale, pop, sub, viz, cont, menu;
     int flag = 0, tryes = 0, escolha = 0;
     char nome_fich[15];
@@ -60,8 +60,10 @@ case 1://TREPA COLINAS COM 2 VIZINHOS
             bestbest = bestmbf = mbf;
             flag = 0;
             }else if(mbf < bestmbf && flag < viz+1)
-              bestmbf = mbf;
+              {
+                  bestmbf = mbf;
               flag += 1;
+              }
             }
             flag = 0;
             setQs();
@@ -95,8 +97,10 @@ case 1://TREPA COLINAS COM 2 VIZINHOS
           bestbest = bestmbf = mbf;
           flag = 0;
         }else if(mbf < bestmbf && flag < viz+1)
-          bestmbf = mbf;
+         {
+             bestmbf = mbf;
           flag += 1;
+         }
         }
       flag = 0;
       setQs();
@@ -230,71 +234,123 @@ case 3: //encontra a melhor solucao entre um numero de interacoes
           break;
 
 case 4: //Pesquisa hibrida
+
 system("clear");
+        if(escolha == 0){
+          printf("Ficheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
+          pd = init_dados(nome_fich, &pop, &sub, &cont);
+            for(i=0;i<tryes;i++){
 
-  if(escolha == 0){
-    printf("Ficheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
-    pd = init_dados(nome_fich, &pop, &sub, &cont);
-      for(i=0;i<tryes;i++){
-        if(flag == 3 && mbf<=bestbest){
-          puts("\n\n******************************************************\n");
-          printf("\tMelhor solucao encontrada: %d",bestbest);
-          puts("\n\n******************************************************\n");
-      }
-    }
-  gera_sol_inicial(pd, cont, pop,sub);
-  mbf = genetico(pd, pop, sub,cont);
-    if(mbf <= bestmbf){
-      bestbest = bestmbf = mbf;
-      flag = 0;
-      }else if(mbf < bestmbf)
-      ale = random_l_h(0,1);
-        if(ale == 1){
-        bestmbf = mbf;
-        flag += 1;
-      }
+        gera_sol_inicial(pd, cont, pop,sub);
+        mbf = genetico(pd, pop, sub,cont);
+          if(mbf >= bestmbf){
+            bestbest = bestmbf = mbf;
+            flag = 0;
+            }else if(mbf < bestmbf)
+            ale = random_l_h(0,1);
+              if(ale == 1){
+              bestmbf = mbf;
+              flag += 1;
+            }
+                     if(flag == 3){
+            puts("\n\n******************************************************\n");
+            printf("\tMelhor solucao encontrada: %d", bestbest);
+            puts("\n\n******************************************************\n");
+            i = tryes;
+            }
+          }
 
-  flag = 0;
-  setQs();
-  }else{
-    imprimeFileList();
-    scanf("%s", nome_fich);
-    printf("Ficheiro seleccionado: %s\n", nome_fich);
-    puts("(100 iteracoes por defeito)\nNumero de iteracoes desejadas? : \n");
-    printf("Digite '0' para default: ");
-    scanf("%d", &tryes);
-    if(tryes == 0){
-      tryes = 100;
-    }
-system("clear");
-printf("\t\t\t\t\tFicheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
-do{
-//  pd = init_dados(nome_fich, &pop, &sub, &cont);
-  mbf_aux = genetico(pd, pop, sub,cont);
-    if(mbf_aux >= bestmbf){
-      bestbest = bestmbf = mbf_aux;
-      flag = 0;
-    }else if(mbf_aux < bestmbf && flag < 3)
-      ale = random_l_h(0,1);
-        if(ale == 1){
-          bestmbf = mbf_aux;
-          flag += 1;
+        flag = 0;
+        setQs();
+        free(pd);
+        bestmbf = mbf = 0;
+        mbf_aux = bestbest;
+        bestbest = 0;
+        }else{
+          imprimeFileList();
+          scanf("%20s", nome_fich);
+          printf("Ficheiro seleccionado: %s\n", nome_fich);
+          puts("(100 iteracoes por defeito)\nNumero de iteracoes desejadas? : \n");
+          printf("Digite '0' para default: ");
+          scanf("%d", &tryes);
+          if(tryes == 0){
+            tryes = 100;
+          }
+      system("clear");
+      printf("\t\t\t\t\tFicheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
+
+      pd = init_dados(nome_fich, &pop, &sub, &cont);
+        for(i=0;i<tryes;i++){
+
+      gera_sol_inicial(pd, cont, pop,sub);
+      mbf = genetico(pd, pop, sub,cont);
+        if(mbf >= bestmbf){
+          bestbest = bestmbf = mbf;
+          flag = 0;
+        }else if(mbf < bestmbf)
+          ale = random_l_h(0,1);
+            if(ale == 1){
+              bestmbf = mbf;
+              flag += 1;
+              }
+              if(flag == 3){
+            puts("\n\n******************************************************\n");
+            printf("\tMelhor solucao encontrada: %d", bestbest);
+            puts("\n\n******************************************************\n");
+            i = tryes;
+            }
+}
+          flag = 0;
+          setQs();
+          free(pd);
+          bestmbf = mbf = 0;
+          mbf_aux = bestbest;
+          bestbest = 0;
         }
-      mbf_aux = genetico(pd, pop, sub,cont);
-        if(mbf_aux > bestmbf){
-          bestmbf = mbf_aux;
-        }
+          escolha = 1;
+////////////////////////////////////////////////////////
 
-  }while (bestbest < bestmbf);
-    puts("\n\n******************************************************\n");
-    printf("\tMelhor solucao encontrada: %d", mbf_aux);
-    puts("\n\n******************************************************\n");
-    }
-    flag = 0;
-    mbf = 0;
-    mbf_aux = 0;
-    escolha = 1;
-    break;
+       system("clear");
+        if(escolha == 0){
+          printf("Ficheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
+          pd = init_dados(nome_fich, &pop, &sub, &cont);
+          for(i=0;i<tryes;i++){
+            gera_sol_inicial(pd, cont, pop,sub);
+            mbf = genetico(pd, pop, sub,cont);
+              if(mbf > bestmbf){
+                bestmbf = mbf;
+              }
+            }
+          setQs();
+          free(pd);
+          mbf = 0;;
+        }else{
+          system("clear");
+          printf("Ficheiro seleccionado: %s, com %d iteracoes.\n\n", nome_fich, tryes);
+          pd = init_dados(nome_fich, &pop, &sub, &cont);
+          for(i=0;i<tryes;i++){
+            gera_sol_inicial(pd, cont, pop,sub);
+            mbf = genetico(pd, pop, sub,cont);
+              if(mbf > bestmbf){
+                bestmbf = mbf;
+              }
+            }
+
+          setQs();
+          free(pd);
+          mbf = 0;
+        }
+          escolha = 1;
+          getchar();
+          puts("\n\n******************************************************\n");
+          printf("\tMelhor solucao encontrada Trepa-colinas prob 50%%: %d\n", mbf_aux);
+          puts("\n******************************************************\n");
+          printf("\tMelhor solucao encontrada Trepa-colinas vizinhos: %d\n", bestmbf);
+          puts("\n******************************************************\n");
+
+
+          break;
+
 case 5:
         system("clear");
         puts("\n\n******************************************************\n");
